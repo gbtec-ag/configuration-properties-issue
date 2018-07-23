32306e50ede1405883da3090565c914c9c5c90fc
Jan Lengenfeld <jan.lengenfeld@gbtec.de>
2018-07-23 15:57:58 +0200

    refactor: remove unnecessary identifier

--- src/main/java/com/example/demo/feign/ExampleFeignClient.java
+++ src/main/java/com/example/demo/feign/ExampleFeignClient.java
@@ -8,5 +8,5 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public interface ExampleFeignClient {
 
     @RequestMapping("/posts/1")
-    public User getUser();
+    User getUser();
 }
